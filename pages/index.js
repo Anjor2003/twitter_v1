9cc7d19db9388f85930c2d986d711eae0ddae715
Anjor2003 <anjor2003@hotmail.com>
2023-12-05 13:51:18 +0100

    corregido errores 3

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import Head from "next/head";
 import CommentModal from "@/components/CommentModal";
 import Feed from "@/components/Feed";
 import Sidebar from "@/components/Sidebar";
 import Widgets from "@/components/Widgets";
-import Head from "next/head";
 
 export default function Home({ newsResults, randomUsersResults }) {
   return (
